d1823471ff18cb1ccda0f1c3317eebc4a147585e
Nikita Pavlov/Vega PC <zoomzoomernik@gmail.com>
2024-01-16 13:36:32 +0900

    3 excersises passed, 4 started; first 2 tests work

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let finalSum = 0;
 
+    for (let i = a; i != b + 1; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
